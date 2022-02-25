364ae5dda341474c2b5f6363f34c6bbac96752f9
Michaeline Goh <taifen_g@yahoo.com>
2022-02-25 16:12:54 +1300

    Fixed css style and validation code

--- register.php
+++ register.php
@@ -1,61 +1,75 @@
 <?php
+// Database Connection.
+include 'config.php';
 
-$showAlert = false;
-$showError = false;
-$exists = false;
+$showSuccess = false;
+$showErrors = array();
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if (isset($_POST['register_submit'])) {
 
     // Include file which makes the
-    // Database Connection.
-    include 'config.php';
 
-    $user_email = $_POST["email"];
+    $user_fullname = $_POST["fullname"];
     $password = $_POST["password"];
     $cpassword = $_POST["cpassword"];
-    $fullname = $_POST["fullname"];
+    $user_email = $_POST["email"];
 
+    
 
-    $sql = "Select * from users where user_email='$user_email'";
+    // validated users fields
 
-    $result = mysqli_query($link, $sql);
-
-    $num = mysqli_num_rows($result);
-
-    // This sql query is use to check if
-    // the username is already present 
-    // or not in our Database
-    if ($num == 0) {
-        if (($password == $cpassword) && $exists == false) {
-
-            $hash = password_hash(
-                $password,
-                PASSWORD_DEFAULT
-            );
-
-            // Password Hashing is used here. 
-            $sql = "INSERT INTO `users` ( `user_email`, 
-                    `password_hash`, `user_fullname`, `user_role`) VALUES ('$user_email', 
-                    '$hash', '$fullname', 'customer')";
-
-            $result = mysqli_query($link, $sql);
-
-            if (mysqli_error($link)) {
-                echo mysqli_error($link);
-            }
-
-            if ($result) {
-                $showAlert = true;
-                header("Location:login.php");
-            }
-        } else {
-            $showError = "Passwords do not match";
-        }
-    } // end if 
-
-    if ($num > 0) {
-        $exists = "This email already exists and not available. Please choose another one.";
+    if (empty($user_fullname)) {
+        $showErrors[] = "You have not entered a fullname";
     }
+    if (empty($password)) {
+        $showErrors[] = "Please enter the password";
+    } elseif($password !== $cpassword) {
+        $showErrors[] = "Passwords do not match";
+    } 
+
+    if (empty($user_email)) {
+       $showError = "You have not entered an email address";
+    } elseif (!filter_var($user_email, FILTER_VALIDATE_EMAIL)) {
+        $showErrors[] = "You have not entered a valid address";
+    }
+    if (empty($showErrors)) {
+        $query = "Select * from users where user_email='$user_email'";
+
+        $result = mysqli_query($link, $query);
+
+        $row = mysqli_num_rows($result);
+        // This sql query is use to check if
+        // the username is already present 
+        // or not in our Database 
+
+        if ($row > 0) {
+            $showErrors[] = "This email already exists and not available. Please choose another one.";
+        }
+    }
+    if (empty($showErrors)) {
+
+        $hash = password_hash(
+            $password,
+            PASSWORD_DEFAULT
+        );
+        // Password Hashing is used here. 
+
+        $query = "INSERT INTO `users` ( `user_email`, 
+                    `password_hash`, `user_fullname`, `user_role`) VALUES ('$user_email', 
+                    '$hash', '$user_fullname', 'customer')";
+
+        $result = mysqli_query($link, $query);
+
+        if (mysqli_error($link)) {
+            echo mysqli_error($link);
+        }
+
+        if ($result) {
+            $showSuccess = true;
+            header("Location:login.php");
+        }
+    } 
+
 } //end if   
 
 ?>
@@ -90,35 +104,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </div>
         <!-- end of breadcrumbs -->
         <!-- show error -->
-        <?php
-
-        if ($showAlert) {
-            echo ' <div class="alert alert-success alert-dismissible fade show" role="alert">
-            <strong>Success!</strong> Your account is now created and you can login. 
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close"> 
-            <span aria-hidden="true">×</span> 
-            </button> 
-            </div> ';
-        }
-
-        if ($showError) {
-            echo ' <div class="alert alert-danger alert-dismissible fade show" role="alert"> 
-                        <strong>Error!</strong> ' . $showError . '
-                        <button type="button" class="close" data-dismiss="alert aria-label="Close">
-                        <span aria-hidden="true">×</span> 
-                        </button> 
-                    </div> ';
-        }
-
-        if ($exists) {
-            echo ' <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        <strong>Error!</strong> ' . $exists . '
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"> 
-                        <span aria-hidden="true">×</span> 
-                        </button>
-                    </div> ';
-        }
-        ?>
+        <div class="container container-error-wrapper">
+            <ul id="errors" class="<?php
+                if(!empty($showErrors)) echo $showErrors ? 'visible' : ''; ?>">
+                <li id="info">There were some problems with your registration:</li>
+                <?php
+                if (!empty($showErrors)) :
+                    foreach ($showErrors as $showError) :
+                ?>
+                <li>
+                    <?php echo $showError ?></li>
+                <?php
+                    endforeach;
+                endif;
+                ?>
+            </ul>
+            <p id="success" class="<?php 
+                if (!$showErrors) echo $showSuccess ? 'visible' : ''; ?>">Thanks for your message! We will get back to you ASAP!</p>
+        </div>
          <!-- end of show error  -->
         <div class="container">
                 <h1 class="text-center">Register Here</h1>
@@ -127,40 +130,45 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     <div class="col-6 register-left-frame">
                         <form action="register.php" method="post">
                             <div class="form-group">
-                                <label for="user_fullname">Name</label>
-                                <input type="text" class="form-control" id="fullname" name="fullname" placeholder="Fullname" aria-describedby="emailHelp">
+                                <label for="fullname">Fullname</label>
+                                <input type="text" class="form-control" id="fullname" name="fullname" placeholder="Fullname" pattern=[A-Z\sa-z]{3,20} autofocus="autofocus"required="required">
+                                <span class="error" aria-live="polite"></span>
                             </div>
                             <div class="form-group">
                                 <label for="email">Email</label>
-                                <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+                                <input type="email" class="form-control" id="email" name="email" placeholder="Email" required="required">
+                                <span class="error" aria-live="polite"></span>
                             </div>
 
                             <div class="form-group">
                                 <label for="password">Password</label>
-                                <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                                <input type="password" class="form-control" id="password" name="password" placeholder="Password" required="required">
+                                <span class="error" aria-live="polite"></span>
                             </div>
 
                             <div class="form-group">
                                 <label for="cpassword">Confirm Password</label>
-                                <input type="password" class="form-control" id="cpassword" name="cpassword">
+                                <input type="password" class="form-control" id="cpassword" name="cpassword" required="required">
+                                <span class="error" aria-live="polite"></span>
 
                                 <small id="emailHelp" class="form-text text-muted">
                                     Make sure to type the same password
                                 </small>
                             </div>
 
-                            <button type="submit" class="btn btn-outline-success btn-block">
+                            <button id="register_submit" type="submit" name="register_submit"class="btn btn-outline-success btn-block">
                                 Register
                             </button>
                         </form>
+                        <?php unset($_SESSION['register_submit']); ?>
                     </div>
                     <div class="col-6 register-right-frame">
-                        <div class="row d-flex">
-                            <div class="col-10 register-logo-square">
+                        <div class="row justify-content-center">
+                            <div class="mt-5 text-center">
                                 <img class="registerPageLogo" src="images/registerPageLogo.svg" alt="" />
-                                <h3 class="registerTitle">Register</h3>
-                                <h5 class="registerTexts">Welcome to Carland</h5>
-                                <ul class="nav justify-content-center registerPageIcons">
+                                <div class="text-center mt-5 registerTitle">Register</div>
+                                <div class="text-center mt-3 registerTexts">Welcome to Carland</div>
+                                <ul class="nav justify-content-center mt-5 registerPageIcons">
                                     <li class="nav-item navRegisterIcon"><i class="bi bi-facebook"></i></li>
                                     <li class="nav-item navRegisterIcon"><i class="bi bi-twitter"></i></li>
                                     <li class="nav-item navRegisterIcon"><i class="bi bi-linkedin"></i></li>
