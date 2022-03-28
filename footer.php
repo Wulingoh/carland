95c064410302b8a1b292f312296d2a685e9a5dd8
Michaeline Goh <taifen_g@yahoo.com>
2022-03-28 22:09:16 +1300

    Fixed and used relative path for href and src

--- footer.php
+++ footer.php
@@ -5,17 +5,17 @@
     <footer class="page-footer font-small">
         <!-- footer vector svg top -->
         <div class="vecto-shade">
-            <img class="shade" src="/images/shade.png" />
+            <img class="shade" src="images/shade.png" />
         </div>
         <div class="vector-images mt-5">
-            <img class="vector mt-5" src="/images/Vector.png" />
+            <img class="vector mt-5" src="images/Vector.png" />
         </div>
         <!-- end of footer vector svg top -->
         <!-- right side footer -->
         <div class="container-fluid footer-container">
             <div class="container">
                 <div class="row">
-                    <a class="navbar-brand footer-logo" href="#"><img src="/images/logoWhite.svg" /></a>
+                    <a class="navbar-brand footer-logo" href="#"><img src="images/logoWhite.svg" /></a>
                 </div>
             </div>
             <div class="clearfix"></div>
@@ -24,24 +24,24 @@
                     <div class="col-8 d-flex">
                         <ul class="col-md-3 list-unstyled footer-nav">
                             <li class="font-weight-bolder">
-                                <a href="/productsListing.php">Buy</a>
+                                <a href="productsListing.php">Buy</a>
                             </li>
                         </ul>
                         <ul class="col-md-2 list-unstyled footer-nav">
                             <li class="font-weight-bolder">
-                                <a href="/aboutPage.php">About Us</a>
+                                <a href="aboutPage.php">About Us</a>
                             </li>
                             <li>
-                                <a href="/aboutPage.php/#ourStory" target="_blank">Our Story</a>
+                                <a href="aboutPage.php/#ourStory" target="_blank">Our Story</a>
                             </li>
                             <li>
-                                <a href="/aboutPage.php/#ourMission" target="_blank">Our Mission</a>
+                                <a href="aboutPage.php/#ourMission" target="_blank">Our Mission</a>
                             </li>
                             <li>
-                                <a href="/aboutPage.php/#ourValues" target="_blank">Our Values</a>
+                                <a href="aboutPage.php/#ourValues" target="_blank">Our Values</a>
                             </li>
                             <li>
-                                <a href="/aboutPage.php/#ourTeam" target="_blank">Our Team</a>
+                                <a href="aboutPage.php/#ourTeam" target="_blank">Our Team</a>
                             </li>
                         </ul>
                         <ul class="col-md-4 list-unstyled footer-nav">
@@ -55,7 +55,7 @@
                     <div class="col-4">
                         <div class="container">
                             <div class="row">
-                                <span class="footer-text">Get <a href="/index.php">Carland</a> on the go</span>
+                                <span class="footer-text">Get <a href="index.php">Carland</a> on the go</span>
                             </div>
                         </div>
                         <div class="container-fluid">
@@ -63,9 +63,9 @@
                                 <ul class="list-group list-group-horizontal-sm">
                                     <!-- <li class="list-group-item">An item</li> -->
                                     <li class="list-group-item borderless"><a class="footer_image" href="#"><img
-                                                src="/images/googlePlayBadge.svg" /></a></li>
+                                                src="images/googlePlayBadge.svg" /></a></li>
                                     <li class="list-group-item borderless"><a class="footer_image" href="#"><img
-                                                src="/images/appleStoreBadge.svg" /> </li>
+                                                src="images/appleStoreBadge.svg" /> </li>
                                 </ul>
                             </div>
                         </div>
@@ -76,27 +76,27 @@
                                     <div class="row">
                                         <ul class="list-unstyled flexbox">
                                             <li class="list-inline-item">
-                                                <a href="/contact.php" class="nav-link footer-icons" href="#">
+                                                <a href="contact.php" class="nav-link footer-icons" href="#">
                                                     <i class="bi bi-mailbox" style="font-size: 25px"></i>
                                                 </a>
                                             </li>
                                             <li class="list-inline-item">
-                                                <a href="/contact.php" class="nav-link footer-icons" href="#">
+                                                <a href="contact.php" class="nav-link footer-icons" href="#">
                                                     <i class="bi bi-phone-vibrate" style="font-size: 25px"></i>
                                                 </a>
                                             </li>
                                             <li class="list-inline-item">
-                                                <a href="/contact.php" class="nav-link footer-icons" href="#">
+                                                <a href="contact.php" class="nav-link footer-icons" href="#">
                                                     <i class="bi bi-facebook" style="font-size: 21px"></i>
                                                 </a>
                                             </li>
                                             <li class="list-inline-item">
-                                                <a href="/contact.php" class="nav-link footer-icons" href="#">
+                                                <a href="contact.php" class="nav-link footer-icons" href="#">
                                                     <i class="bi bi-linkedin" style="font-size: 21px"></i>
                                                 </a>
                                             </li>
                                             <li class="list-inline-item">
-                                                <a href="/contact.php" class="nav-link footer-icons" href="#">
+                                                <a href="contact.php" class="nav-link footer-icons" href="#">
                                                     <i class="bi bi-twitter" style="font-size: 21px"></i>
                                                 </a>
                                             </li>
